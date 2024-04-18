ca5419ca347f6a88260e4df861bdec4850983b6a
daneshswara29 <44220709+daneshswara29@users.noreply.github.com>
2024-04-19 03:13:10 +0700

    Update jam.sh

--- jam.sh
+++ jam.sh
@@ -34,7 +34,7 @@ echo -e "     ${green}Current : $aureb $gg ${NC}"
 echo -e ""
 echo -e " ${BICyan}└───────────────────────────────────────────────┘${NC}" 
 echo -e "${BICyan}┌────────────────────── BY ───────────────────────┐${NC}"
-echo -e "${BICyan}│${NC}             • CorpIDR2 VPN •           ${BICyan}│$NC"
+echo -e "${BICyan}│${NC}             • DANESHSWARA •           ${BICyan}│$NC"
 echo -e "${BICyan}└─────────────────────────────────────────────────┘${NC}"
 
 while :; do
